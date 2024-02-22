c4182b1a389cda566e37d8bdeb6cf906e5a63c43
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-02-22 07:29:44 +0530

    completed the UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,4 +99,9 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -3,44 +3,72 @@ package com.example.roomdatabase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.roomdatabase.Model.NoteDatabase
+import com.example.roomdatabase.ViewModel.AddNoteScreen
+import com.example.roomdatabase.ViewModel.NoteViewModel
+import com.example.roomdatabase.ViewModel.NotesScreen
 import com.example.roomdatabase.ui.theme.RoomDatabaseTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val database by lazy{
+        Room.databaseBuilder(
+            applicationContext,
+            NoteDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+
+    private val viewModel: NoteViewModel by viewModels<NoteViewModel> (
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return NoteViewModel(database.dao) as T
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RoomDatabaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val state by viewModel.state.collectAsState()
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "notesScreen"){
+                    composable("notesScreen"){
+                        NotesScreen(
+                            state = state,
+                            navController = navController,
+                            onEvent = viewModel::onEvent
+                        )
+                    }
+                    composable("addNotesScreen"){
+                        AddNoteScreen(
+                            state = state,
+                            navController = navController,
+                            onEvent = viewModel::onEvent
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoomDatabaseTheme {
-        Greeting("Android")
-    }
-}
